2a5268492372c1cbae4330f5a341d933e06c52c5
Cameron Brown <cameronjbrown@gmail.com>
2016-08-23 13:46:30 -0700

    Added new function without a test to intentionally reduce coverage

--- hello/util.py
+++ hello/util.py
@@ -1,4 +1,11 @@
+"""Fake utilitites for testing testing"""
 
-def add_two(x):
-    return x + 2
 
+def add_two(num):
+    """Add two to num"""
+    return num + 2
+
+
+def add_three(num):
+    """Add three to num"""
+    return num + 3
